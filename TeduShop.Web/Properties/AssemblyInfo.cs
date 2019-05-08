60a1aa4e6921cf3e2a80daf46765820431d10a58
Dell-Henry <phamtiensy8696@gmail.com>
2019-05-08 22:46:11 +0700

    Database first

--- TeduShop.Web/Properties/AssemblyInfo.cs
+++ TeduShop.Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TeduShop.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b6b9ac53-0ca9-4488-b4e4-817762b8de62")]
+[assembly: Guid("46646acf-a520-4f0b-b7bc-7c48c6beeb61")]
 
 // Version information for an assembly consists of the following four values:
 //
